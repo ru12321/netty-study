00c62a1b48bc6141bcd83ba71e4ab9aacb6c1919
ru12321 <1075348489@qq.com>
2021-07-31 19:04:52 +0800

    push commit

---
+++ src/main/java/com/mrru/TestGit.java
@@ -0,0 +1,14 @@
+package com.mrru;
+
+/**
+ * @className: TestGit
+ * @author: 茹某
+ * @date: 2021/7/31 19:03
+ **/
+public class TestGit
+{
+    public static void main(String[] args)
+    {
+        System.out.println("git 配合idea 使用");
+    }
+}
